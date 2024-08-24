ef7a1e69f255670b96ad864bc29ca5d73f2a5a29
KASHINATH <150243129+KASHI044@users.noreply.github.com>
2024-08-24 23:51:52 +0530

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,8 @@
+-- Insert initial data into suppliers table
+INSERT INTO suppliers (id, name, location, nature_of_business, manufacturing_processes) VALUES
+(1, 'ABC Suppliers', 'India', 'small_scale', 'COATING'),
+(2, 'XYZ Industries', 'India', 'small_scale', 'COATING'),
+(3, 'Global Suppliers', 'USA', 'large_scale', 'CASTING'),
+(4, 'TechManufacturing', 'India', 'medium_scale', 'MOULDING'),
+(5, 'EcoSupplies', 'Germany', 'small_scale', 'COATING'),
+(6, 'QuickManufacture', 'India', 'small_scale', 'COATING');
